ef62faacb346af2c12f6714d061e0a967eeea2f9
Lukas Fülling <k3ll4gr0up@gmail.com>
2014-03-27 19:47:46 +0100

    finished comments and added info
    Call it via --info

--- main.c
+++ main.c
@@ -45,23 +45,25 @@ int main(int argc, char *argv[]){
 	int fuel;			/* The fuel you have left. (kilogram) */
 	int prevheight;			/* The previous height to compare with actual. (coloured digits) */
 	int step;			/* Counts the steps passed since last output of the collumn names */
-
-
+	
+	
+	char version[]="1.1";		/* The Version of the program */
 	char dead[]="\nThere were no survivors.\n\n";
 	char crashed[]="\nThe Spaceship crashed. Good luck getting back home.\n\n";
 	char success[]="\nYou made it! Good job!\n\n";
 	char emptyfuel[]="\nThere is no fuel left. You're floating around like Wheatley.\n\n";
 	
-	printf("\nLunar Lander - Version 1.0\n");
+	printf("\nLunar Lander - Version %s\n", version);
 	printf("This is a computer simulation of an Apollo lunar landing capsule.\n");
 	printf("The on-board computer has failed so you have to land the capsule manually.\n");
 	printf("Set burn rate of retro rockets to any value between 0 (free fall) and 200\n");
 	printf("(maximum burn) kilo per second. Set burn rate every 10 seconds.\n"); /*that's wy we have to go with 10 second-steps.*/
 	printf("Good Luck!\n");
 	
-		/* Set initial height, time, fuel, burn, prevheight, step and speed properly to difficulty. */
-	if(argc==1){
-		speed=1000;
+		/* Set initial height, time, fuel, burn, prevheight, step and speed according to difficulty. */
+		
+	if(argc==1){					/* If there is only one argument (which is the program's name) */
+		speed=1000;				/* Default to easy (and randomize the height) */
 		height=randomheight();
 		fuel=12000;
 		tensec=0;
@@ -69,8 +71,8 @@ int main(int argc, char *argv[]){
 		prevheight=height;
 		step=1;
 	}
-	else {
-		if(strcmp(argv[1], "-d")==0){
+	else {						/* If there are more arguments (or less) */
+		if(strcmp(argv[1], "-d")==0){		/* If the "first" Argument is -d, check for the second argument*/
 			if(strcmp(argv[2], "1")==0){	/* Easy */
 				speed=1000;
 				height=randomheight();
@@ -98,7 +100,7 @@ int main(int argc, char *argv[]){
 				prevheight=height;
 				step=1;
 			}
-			else {					/* Default, Easy */
+			else {					/* If argv[1] is not -d, default to Easy */
 				speed=1000;
 				height=randomheight();
 				fuel=12000;
@@ -108,15 +110,20 @@ int main(int argc, char *argv[]){
 				step=1;
 			}
 		}	
-			else {					/* Default, Easy */
-				speed=1000;
-				height=randomheight();
-				fuel=12000;
-				tensec=0;
-				burn=0;
-				prevheight=height;
-				step=1;
-			}
+		else if(strcmp(argv[1], "--info")==0){
+			printf("\nLunar Lander - Version %s\n", version);
+			printf("\nThis is a school project made by Nicolai Sueper and Lukas Fuelling.");
+			printf("\n\nContact us at http://k40s.net")
+		}
+		else {					/* If the first Argument is something else, default to Easy */
+			speed=1000;
+			height=randomheight();
+			fuel=12000;
+			tensec=0;
+			burn=0;
+			prevheight=height;
+			step=1;
+		}
 	}	
 	
 	
